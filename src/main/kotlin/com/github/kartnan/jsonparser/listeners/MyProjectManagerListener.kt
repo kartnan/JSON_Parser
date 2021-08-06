1d05ac0c0ee546ddc597507d5349cbe317dab758
GitHub Action <action@github.com>
2021-08-06 14:07:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/kartnan/jsonparser/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/kartnan/jsonparser/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kartnan.jsonparser.listeners
 
+import com.github.kartnan.jsonparser.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/kartnan/jsonparser/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kartnan/jsonparser/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kartnan.jsonparser.services
 
+import com.github.kartnan.jsonparser.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
